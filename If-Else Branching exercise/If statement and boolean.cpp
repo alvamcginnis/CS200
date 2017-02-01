07f03803299d8002e4cd6e9ed6e6a537722fda20
Maryam Albakhiet <malbkhiet7@gmail.com>
2017-01-31 21:11:34 -0600

    Add files via upload

---
+++ If-Else Branching exercise/If statement and boolean.cpp
@@ -0,0 +1,29 @@
+// If statement and boolean.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std; 
+
+/* When I try likesPizza on its own : if it's true it will show 1 in the output
+and if it's false it will show 0 in the output
+*/
+
+int main()
+
+{
+	bool likesPizza;
+
+	if (likesPizza = true)
+
+		cout << "likes Pizza. ";
+
+	else
+		cout << " Doesn't like pizza. ";
+
+	
+
+	while (true);
+	return 0;
+}
+
