fdd13dac298270fd6f531c4b31ec60e2cc056a44
Maryam Albakhiet <malbkhiet7@gmail.com>
2017-02-05 21:40:27 -0600

    Add files via upload

---
+++ For loops/0 to 20.cpp
@@ -0,0 +1,20 @@
+// 0 to 20.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+
+
+int main()
+{
+	for (int i = 0; i < 20; ++i)
+	{
+		cout << i << " ";
+	}
+
+    return 0;
+}
+
