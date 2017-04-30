3a6bb4608177d8bc14681d5679d98fd1019343f3
Maryam Albakhiet <malbkhiet7@gmail.com>
2017-04-29 19:48:42 -0500

    Add files via upload

---
+++ Lab 16 MoreWithClasses/Button.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include "Button.hpp"
+
+void Button::SetText(string text)
+{
+	m_text = text;
+}
+
+void const Button::Draw()
+{
+	int len = m_text.size() + 4;
+
+	for (int i = 0; i < len; i++)
+	{
+		cout << "*";
+	}
+	cout << endl;
+	cout << "* " << m_text << " *" << endl;
+
+	for (int i = 0; i < len; i++)
+	{
+		cout << "*";
+	}
+	cout << endl;
+}
